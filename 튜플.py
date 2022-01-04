0030a4be1c56e0108b6a8e85c0db779ebb4e3c3d
wnshrla <wnshrla@naver.com>
2022-01-04 10:18:40 +0900

    2019 카카오개발자 겨울 인턴십

--- 튜플.py
+++ 튜플.py
@@ -1,3 +1,5 @@
+# 풀이 1
+
 def solution(s):
     s = s.split("}")
     for i in range(len(s)):
@@ -15,3 +17,30 @@ def solution(s):
                 answer.append(int(j))
 
     return answer
+
+# 더 좋은 풀이 2
+'''
+
+Python - String strip(), rstrip(), lstrip() 사용
+
+'''
+
+def solution(s):
+    answer = []
+
+    s1 = s.lstrip('{').rstrip('}').split("},{")
+
+    new_s = []
+    for i in s1:
+        new_s.append(i.split(','))
+
+    new_s.sort(key = len)
+
+    for i in new_s:
+        for j in range(len(i)):
+            if int(i[j]) not in answer:
+                answer.append(int(i[j]))
+
+    return answer
+
+# print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
