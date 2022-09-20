93e8d5b7b0b5849f15e99590379b468805bfd086
wnshrla <wnshrla@naver.com>
2022-09-20 13:03:15 +0900

    2021카카오

--- 메뉴리뉴얼.py
+++ 메뉴리뉴얼.py
@@ -1,44 +1,47 @@
-from collections import defaultdict
 from itertools import combinations
 def solution(orders, course):
-    alpha = defaultdict(int)
-    
+    answer = []
+    new_orders = []
     for i in orders:
-        for j in i:
-            alpha[j] += 1
+        tmp = list(i)
+        tmp.sort()
+        new_orders.append(tmp)
     
-    case = []
-    for i in alpha:
-        if alpha[i] >= 2:
-            case.append(i)
-    
-    answer = defaultdict(list)
-    for i in course: # 2
-        temp = list(combinations(case, i)) 
-        for j in temp: # ('A', 'B')
-            ans = 0
-            for k in orders: # "ABCFG"
-                eacape = False
-                for z in j: # A
-                    if z in k:
-                        escape = True
-                    else:
-                        escape = False
+    max_orders = len(max(orders,key = lambda x:len(x)))
+    for num in course:
+        if max_orders < num:
+            continue
+        ex = []
+        for i in new_orders:
+            ex.extend(list(combinations(i, num)))
+        cnt_dict = {}
+        result = []
+        for num_ex in ex: # (A, B)
+            cnt = 0
+            for order in new_orders: # [A,B,C,F,G]
+                for_cnt = True
+                for j in num_ex:
+                    if j not in order:
+                        for_cnt = False
                         break
-                if escape:
-                    ans += 1
-            if ans >= 2:
-                answer[i].append([j, ans])
-    result = []
-    for i in answer:
-        answer[i].sort(key = lambda x:-x[1])
-        end = answer[i][0][1]
-        for j in answer[i]:
-            if end != j[1]:
+                if for_cnt:
+                    cnt += 1
+            num_ex_str = "".join(num_ex)
+            if cnt >= 2:
+                cnt_dict[num_ex_str] = cnt
+        cnt_dict = sorted(cnt_dict.items(), key=lambda x:x[1], reverse=True)
+        if len(cnt_dict):
+            start = cnt_dict[0][1]
+        else:
+            continue
+        for i in cnt_dict:
+            if start == i[1]:
+                result.append(i[0])
+            else:
                 break
-            t = list(j[0])
-            t.sort()
-            result.append(''.join(t))
-    
-    result.sort()
-    return result
+        
+        for i in result:
+            answer.append(i)
+
+    answer.sort()
+    return answer
