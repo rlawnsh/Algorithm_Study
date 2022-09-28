797d4b491f56253b297935c8f4beeb58d391ba86
wnshrla <wnshrla@naver.com>
2022-09-28 14:27:07 +0900

    DP(미해결)

---
+++ 9251번_LCS.py
@@ -0,0 +1,32 @@
+# import sys
+# read = sys.stdin.readline
+
+# word1, word2 = read().strip(), read().strip()
+# h, w = len(word1), len(word2)
+# cache = [[0] * (w+1) for _ in range(h+1)]
+
+# for i in range(1, h+1):
+#     for j in range(1, w+1):
+#         if word1[i-1] == word2[j-1]:
+#             cache[i][j] = cache[i-1][j-1] + 1
+#         else:
+#             cache[i][j] = max(cache[i][j-1], cache[i-1][j])
+# print(cache[-1][-1])
+
+###############################################################
+
+import sys
+read = sys.stdin.readline
+
+word1, word2 = read().strip(), read().strip()
+l1, l2 = len(word1), len(word2)
+cache = [0] * l2
+
+for i in range(l1):
+    cnt = 0
+    for j in range(l2):
+        if cnt < cache[j]:
+            cnt = cache[j]
+        elif word1[i] == word2[j]:
+            cache[j] = cnt + 1
+print(max(cache))
